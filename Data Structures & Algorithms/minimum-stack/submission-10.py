82931d3d41569699b14361b4d8d0410bec07ad75
Omletta <128774128+Omletta@users.noreply.github.com>
2026-09-19 05:25:02 +0200

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.py
@@ -0,0 +1,36 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = float('inf')
+        self.prev_min_stack = []
+        
+
+    def push(self, val: int) -> None:
+        if val <= self.min : 
+            self.prev_min_stack.append(self.min)
+            self.min = val
+            
+            
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        
+        if self.stack[-1]== self.min  :
+
+            
+            
+            self.min = self.prev_min_stack.pop()
+            
+       
+        self.stack.pop()
+        
+        
+
+    def top(self) -> int:
+        
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min
